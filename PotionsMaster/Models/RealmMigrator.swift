5b678f8515468c7ca38d070d48973a42b4b44a12
Yonghyun Kim <lygon55555@gmail.com>
2022-03-04 14:00:06 +0900

    created migration

--- PotionsMaster/Models/RealmMigrator.swift
+++ PotionsMaster/Models/RealmMigrator.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2020 Razeware LLC
-///
+/// Copyright (c) 2022 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -30,30 +30,20 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
-import SwiftUI
 import RealmSwift
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
-
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    
-    RealmMigrator.setDefaultConfiguration()
-    
-    if let windowScene = scene as? UIWindowScene {
-      do {
-        let realm = try Realm()
-        let window = UIWindow(windowScene: windowScene)
-        
-        let contentView = ContentView()
-          .environmentObject(IngredientStore(realm: realm))
-        window.rootViewController = UIHostingController(rootView: contentView)
-        self.window = window
-        window.makeKeyAndVisible()
-      } catch let error {
-        fatalError("Failed to open Realm. Error: \(error.localizedDescription)")
-      }
+enum RealmMigrator {
+  static private func migrationBlock(migration: Migration, oldSchemaVersion: UInt64) {
+    if oldSchemaVersion < 1 {
+      migration
+        .enumerateObjects(ofType: IngredientDB.className()) { _, newObject in
+          newObject?["colorName"] = "rw-green"
+        }
     }
+  }
+  
+  static func setDefaultConfiguration() {
+    let config = Realm.Configuration(schemaVersion: 1, migrationBlock: migrationBlock)
+    Realm.Configuration.defaultConfiguration = config
   }
 }
